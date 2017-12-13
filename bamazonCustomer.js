3f21f0e54a9f592dbbffc4dbb99a0acf29abdb69
matlittle <matlittle727@gmail.com>
2017-12-12 18:06:09 -0600

    Unknown changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,35 @@
+
 const MySQL = require('mysql');
 const Inquirer = require('inquirer');
 
+const sqlCredentials = require('./sqlCredentials');
+const connection = MySQL.createConnection(sqlCredentials);
+
+
+purchaseProduct().then(function() {
+    console.log("done");
+});
+
+
+async function purchaseProduct() {
+    const productId = await getProductId();
+
+    console.log("Product ID ===============");
+    console.log( productId );
+
+    return;
+}
+
+async function getProductId() {
+    const query = 'SELECT item_id, product_name, price FROM products';
+
+    var result = new Promise(resolve => {
+        connection.query(query, async function(err, res){
+            if(err) reject(err);
+
+            resolve(res);
+        });
+    }, reject => { });
+    
+    return result;
+}
