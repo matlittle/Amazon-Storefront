f659d7d3342a2d6048f4713759351444bc8518a0
matlittle <matlittle727@gmail.com>
2017-12-13 23:48:18 -0600

    Finished customer file, and added manager file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,46 +7,129 @@ const connection = MySQL.createConnection(sqlCredentials);
 
 
 purchaseProduct().then(function() {
-    console.log("done");
+    console.log("\nDone");
 });
 
 
 async function purchaseProduct() {
-    const productIdArr = await getProductId().catch( (err) => {
+    const productArr = await getProductList().catch( (err) => {
         console.error(err)
     });
 
-    var productDisplayArr = [];
-
-    productIdArr.forEach( (product) => {
-        let str = `ID: ${product.item_id}  |  `
-            + `Name: ${product.product_name}  |  `
-            + `Price: ${product.price}`;
-        productDisplayArr.push(str);
+    const choices = buildProductQuestions(productArr);
+    
+    const answer = await productSelectionPrompt(choices).catch( (err) => {
+        console.error(err);
     });
 
-    console.log("Product ID ================================================");
-    console.log( productIdArr );
+    const product = productArr.find( (obj) => {
+        return obj.item_id === answer.item;
+    });
 
-    console.log("Product Display ===========================================");
-    console.log( productDisplayArr );
+    const numInStock = await checkStock(answer.item);
 
-    
+    if( numInStock > parseInt(answer.count) ) {
+        await performTransaction(answer, numInStock);
+        let total = (parseInt(answer.count) * product.price).toFixed(2);;
+        console.log(`\nYour total will be $${total}\n`);
+    } else {
+        console.log("\nInsufficent quantity in stock!\n");
+    }
 
-    return;
+    const another = await newTransactionPrompt()
+    console.log('\n');
+
+    if(another.bool) {
+        return purchaseProduct();
+    }
+
+    return endConnection();
 }
 
-async function getProductId() {
+async function getProductList() {
     const query = 'SELECT item_id, product_name, price FROM products';
 
-    var result = new Promise( ( resolve, reject ) => {
-        connection.query(query, async function(err, res){
+    return new Promise( ( resolve, reject ) => {
+        connection.query(query, async (err, res) => {
             if(err) reject(err);
 
             resolve(res);
         });
     });
+}
 
-    return result;
+function buildProductQuestions(arr) {
+    let qArr = [];
+    
+    arr.forEach( (p) => {
+        let obj = {
+            name: `${p.product_name.padEnd(15)}   `
+                + `$${p.price.toFixed(2)}`,
+            value: p.item_id
+        };
+
+        qArr.push(obj);
+    });
+
+    return qArr;
+}
+
+async function productSelectionPrompt(choices) {
+    const questions = [{
+        message: "Which item would you like to purchase?",
+        type: "list", 
+        choices: choices,
+        name: "item",
+        pageSize: 15
+    },
+    {
+        message: "How many would you like to purchase?",
+        type: "input", 
+        name: "count"
+    }];
+
+    return Inquirer.prompt(questions);
+}
+
+async function checkStock(id) {
+    const query = 'SELECT stock_quantity FROM products WHERE item_id = ?';
+    
+    return new Promise( ( resolve, reject ) => {
+        connection.query(query, [id], async (err, res) => {
+            if(err) reject(err);
+
+            resolve(res[0].stock_quantity);
+        });
+    });
+}
+
+async function performTransaction(answer, stock) {
+    const query = `UPDATE products
+                    SET stock_quantity=${stock - parseInt(answer.count)}
+                    WHERE item_id=${answer.item}`;
+    
+    return new Promise ( (resolve, reject) => {
+        connection.query(query, async (err, res) => {
+            if(err) reject(err);
+            resolve(true);
+        });
+    });
+}
+
+async function newTransactionPrompt() {
+    const questions = [{
+        message: "Would you like to purchase another item?",
+        type: "confirm", 
+        name: "bool"
+    }];
+
+    return Inquirer.prompt(questions);
+}
+
+
+function endConnection() {
+    connection.end( (err) => {
+        if(err) console.error(err);
+    });
 }
 
---
+++ bamazonManager.js
@@ -0,0 +1,241 @@
+
+const MySQL = require('mysql');
+const Inquirer = require('inquirer');
+
+const sqlCredentials = require('./sqlCredentials');
+const connection = MySQL.createConnection(sqlCredentials);
+
+require('console.table');
+
+
+init()
+
+async function init() {
+    console.log('\n');
+
+    const answer = await getAction();
+
+    await handleAction(answer.action).catch(logError);
+
+    const another = await promptNewAction();
+
+    if(another.bool) {
+        return init();
+    }
+
+    return endConnection();
+}
+
+
+async function getAction() {
+    const actions = buildActionChoices();
+
+    const questions = [{
+        message: "What would you like to do?",
+        type: "list", 
+        choices: actions,
+        name: "action"
+    }];
+
+    return Inquirer.prompt(questions);
+}
+
+function buildActionChoices() {
+    return [{
+        name: "View Products for Sale",
+        value: "products"
+    },{
+        name: "View Low Inventory",
+        value: "low"
+    },{
+        name: "Add to Inventory",
+        value: "addInv"
+    },{
+        name: "Add New Product",
+        value: "addProd"
+    }];
+}
+
+async function handleAction(action) {
+    switch(action) {
+        case "products":
+            const products = await selectAllProducts().catch(logError);
+            return displayTable(products);
+        case "low":
+            const lowInv = await showLowInventory().catch(logError);
+            if(lowInv.length === 0) {
+                return console.log("\nNo items with less than 5 in stock\n");
+            }
+            return displayTable(lowInv);
+        case "addInv":
+            return addInventory();
+        case "addProd":
+            return addNewProduct();
+    }
+}
+
+async function selectAllProducts() {
+    const query = `SELECT * FROM products`;
+
+    return new Promise( (resolve, reject) => {
+        connection.query(query, async (err, res) => {
+            if(err) reject(err);
+            resolve(res);
+        });
+    });
+}
+
+async function showLowInventory() {
+    const query = `SELECT * FROM products WHERE stock_quantity < 5`;
+
+    return new Promise( (resolve, reject) => {
+        connection.query(query, async (err, res) => {
+            if(err) reject(err);
+            resolve(res);
+        });
+    });
+}
+
+async function addInventory() {
+    const productArr = await selectAllProducts().catch(logError);
+    const choices = buildProductList(productArr);
+
+    const answer = await getProductSelection(choices);
+
+    const product = productArr.find( (obj) => {
+        return obj.item_id === answer.item;
+    });
+
+    const newTotal = parseInt(answer.count) + product.stock_quantity;
+
+    await updateInventory(answer.item, newTotal).catch(logError);
+
+    return console.log(`\nInventory added.\n` +
+        `${product.product_name} - New Stock: ${newTotal}\n`);
+}
+
+async function addNewProduct() {
+    const newProduct = await newProductPrompt();
+
+    newProduct.price = parseFloat(newProduct.price);
+    newProduct.stock_quantity = parseInt(newProduct.stock_quantity);
+
+    await insertNewProduct(newProduct);
+
+    return console.log(`\nNew product '${newProduct.product_name}' added.\n`);
+}
+
+function buildProductList(arr) {
+    let qArr = [];
+    
+    arr.forEach( (p) => {
+        qArr.push( {
+            name: `Product: ${p.product_name.padEnd(10)}  |  `
+                + `Stock: ${p.stock_quantity.toString().padEnd(3)}  `,
+            value: p.item_id
+        });
+    });
+
+    return qArr;
+}
+
+async function getProductSelection(choices) {
+    const questions = [{
+        message: "Which item would you like to add inventory to?",
+        type: "list", 
+        choices: choices,
+        name: "item",
+        pageSize: 15
+    },
+    {
+        message: "How much would you like to add?",
+        type: "input", 
+        name: "count"
+    }];
+
+    return Inquirer.prompt(questions);
+}
+
+async function updateInventory(id, total) {
+    const query = 
+    `UPDATE products
+        SET stock_quantity=${total}
+        WHERE item_id=${id}`;
+
+    return new Promise( (resolve, reject) => {
+        connection.query(query, async (err, res) => {
+            if(err) reject(err);
+            resolve(res);
+        });
+    });
+}
+
+async function newProductPrompt() {
+    const questions = [{
+        message: "New product department",
+        type: "input", 
+        name: "department_name"
+    },
+    {
+        message: "New product name",
+        type: "input", 
+        name: "product_name"
+    },
+    {
+        message: "New product price (##.## format)",
+        type: "input", 
+        name: "price"
+    },
+    {
+        message: "New product stock quantity",
+        type: "input", 
+        name: "stock_quantity"
+    }];
+
+    return Inquirer.prompt(questions);
+}
+
+async function insertNewProduct(product) {
+    const query = `INSERT INTO products SET ?`;
+
+    return new Promise( (resolve, reject) => {
+        connection.query(query, [product], (err, res) => {
+            if(err) reject(err);
+            resolve(res);
+        });
+    });
+}
+
+async function promptNewAction() {
+    const question = [{
+        message: "Would you like to do something else?",
+        type: "confirm", 
+        name: "bool"
+    }];
+
+    return Inquirer.prompt(question);
+}
+
+
+function displayTable(table) {
+    console.log('\n');
+    console.table(table);
+    console.log('\n');
+}
+
+function logError(err) {
+    console.error(err);
+}
+
+function endConnection() {
+    connection.end( (err) => {
+        if(err) console.error(err);
+    });
+}
+
+
+
+
+
+
+
